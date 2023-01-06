6b13f3728d5fe9968908facc7aef51211f2dfb23
ZancoFARRELL <2121218@brunel.ac.uk>
2023-01-06 15:31:13 +0000

    Update Shop.js

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -2,23 +2,18 @@ import "./Shop_Style.css"
 export default function Shop() {
   
 
+  
+
+
   return (
     <div>
-      <div id="overlay">
-        <div id="checkout">
-          <button className="checkout_exit" onClick={cart_hide()}>X</button>
-        </div>
-      </div>
-
-
-
       <div className="item_list">
         <nav>
           <ul>
             <li>
-              <a href=".\item_pages\item_frame.js"><img className="item_img" src="D:\AquaPure\src\pages\shop\shop_assets\water_bottle.PNG"></img></a>
+              <a href=".\item_pages\item_frame.js"><img className="item_img" src=".\shop_assets\water_bottle.PNG"></img></a>
               <p className="item_info">AquaPure Sport Bottle Blue</p>
-              <button id="bottle" className="item_quick_add" type="button" onClick={add_cart()}>Quick Add</button>
+              <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
             </li>
           </ul>
         </nav>
@@ -26,4 +21,9 @@ export default function Shop() {
     </div>
   );
 }
+
+
+
+
+
 
