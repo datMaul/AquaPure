906d97d395b62ab80be029f5860412e61b9d46a2
HumzaAhmad03 <102473918+HumzaAhmad03@users.noreply.github.com>
2023-01-06 18:09:37 +0300

    test

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,107 +1,6 @@
 import "./game.css";
 export default function Gmae() {
 
-
-
-
-const questionCard = document.querySelector('.question')
-const choices = Array.from(document.querySelectorAll('.answer-button'))
-const scoreText = document.querySelector('.score')
-const body = document.querySelector('.body')
-
-window.addEventListener('load', gameStart)
-
-var currentQuestion = {}
-var score = 0
-var questionIndex = 0 
-var acceptingAnswer = true
-var questionList = []
-
-const POINTS = 100
-const MAX_QUESTIONS = 3
-
-var questions = [
-    {
-        question: 'what is a',
-        choice1:  'a',
-        choice2:  'b',
-        choice3:  'c',
-        choice4:  'd',
-        answer: 3,
-    },
-    {
-        question: 'what is b',
-        choice1:  'a',
-        choice2:  'aaaab',
-        choice3:  'c',
-        choice4:  'd',
-        answer: 3,
-    },
-    {
-        question: 'what is c',
-        choice1:  'a',
-        choice2:  'bddd',
-        choice3:  'c',
-        choice4:  'd',
-        answer: 3,
-    }
-]
-
-function gameStart(){
-    console.log('e')
-    score = 0 
-    questionIndex = 0
-    questionList = [... questions]
-    nextQuestion()
-}
-
-function nextQuestion(){
-    if(questionList.length === 0 || questionIndex > MAX_QUESTIONS){
-        localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('game-endscreen.js')
-    }
-
-    const questionCounter = Math.floor(Math.random() * questionList.length)
-    currentQuestion = questionList[questionCounter]
-    questionCard.innerText = currentQuestion.question
-    choices.forEach(choice =>{
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
-
-    questionList.splice(questionCounter, 1)
-    acceptingAnswer = true
-}
-
-choices.forEach(selection =>{
-    selection.addEventListener('click', selectAnswer)
-})
-
-function selectAnswer(selection){
-    if(!acceptingAnswer) return
-
-    acceptingAnswer = false
-    const selectedChoice = selection.target
-    const selectedAnswer = selectedChoice.dataset['number']
-
-    var appliedClass = selectedAnswer == currentQuestion.answer ? 'correct' : 'wrong'
-
-    if(appliedClass === 'correct') {
-        score += POINTS
-        scoreText.innerHTML = "Score <br>" + score;
-    }
-
-    selectedChoice.classList.add(appliedClass)
-    body.classList.add(appliedClass)
-    
-    setTimeout(() => {
-        selectedChoice.classList.remove(appliedClass)
-    body.classList.remove(appliedClass)
-        nextQuestion()
-    }, 2000)
-
-}
-
   return (
   <div className="body">
     <div className="green" ></div>
@@ -116,6 +15,7 @@ function selectAnswer(selection){
         </div>
         <div className="score">Score 0</div>
     </div>
+    <script src="gamerunning.js"></script>
   </div>
   );
 }
