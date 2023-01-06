eb2f71144a59a2b81e8f835d37df3deabc327705
Kofi-Group-16 <92590677+Kofi-Yellow-Group-8@users.noreply.github.com>
2023-01-06 14:41:57 +0000

    bodyfix SignUp
    changed body to only apply to sign up, possible link to registration made

--- src/pages/signin/SignIn.js
+++ src/pages/signin/SignIn.js
@@ -1,10 +1,12 @@
 import "./signin.css";
+//import "./SignUp.css" - also add "SignUp.css" to href on 26
 
 export default function SignIn() {
   return (
     <>
   <meta charSet="utf-8" />
   <link rel="stylesheet" href="signin.css" />
+  <body className="bodyS">
   <div className="center">
     <h1>Login</h1>
     <form method="post">
@@ -21,10 +23,11 @@ export default function SignIn() {
       <div className="pass">Forgot Password?</div>
       <input type="submit" defaultValue="Login" />
       <div className="SignIn">
-        Not a member? <a href="#">Signup</a>
-      </div>
+        Not a member? <a href="#">Signup</a> 
+      </div> 
     </form>
   </div>
+  </body>
 </>
   );
   
