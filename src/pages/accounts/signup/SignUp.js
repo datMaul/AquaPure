ccaa90651e827087f2d015a4c1168b64247d02de
Eren Tastepe <2152973@brunel.ac.uk>
2023-01-10 13:58:44 +0300

    Update SignUp.js

--- src/pages/accounts/signup/SignUp.js
+++ src/pages/accounts/signup/SignUp.js
@@ -1,129 +1,188 @@
-import { Link, useNavigate } from 'react-router-dom';
-import './SignUp.css';
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import "./SignUp.css";
 
 export default function SignUp() {
   const navigate = useNavigate();
 
-  function submitForm(form) {
-    form.preventDefault(); // Prevents AutoSubmitting
+  const [User, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    eMail: "",
+    phoneNumber: "",
+    doB: "",
+    password: "",
+    addressLine1: "",
+    addressLine2: "",
+    addressTC: "",
+    addressPostcode: "",
+  });
 
-    const firstName = form.target.firstName.value;
-    const lastName = form.target.lastName.value;
-    const email = form.target.email.value;
-    const areaCode = form.target.areaCode.value;
-    const phoneNumber = form.target.phoneNumber.value;
-    const dateOfBirth = form.target.dateOfBirth.value;
-    const password = form.target.password.value;
-    const confirmPassword = form.target.confirmPassword.value;
-    const addressLine1 = form.target.addressLine1.value;
-    const addressLine2 = form.target.addressLine2.value;
-    const townOrCity = form.target.townOrCity.value;
-    const postcode = form.target.postcode.value;
-    const termsAndConditions = form.target.termsAndConditions.value;
-    const privacyPolicy = form.target.privacyPolicy.value;
+  const {
+    firstName,
+    lastName,
+    eMail,
+    phoneNumber,
+    doB,
+    password,
+    addressLine1,
+    addressLine2,
+    addressPostcode,
+    addressTC,
+  } = User;
 
-    // const areaCodeRegex = /(?:\+?(\d{1,3}))?/;
-    // const phoneNumberRegex = /(\d{3})(\d{3})(\d{4})(?: *x(\d+))?/;
+  const onInputChange = (e) => {
+    setUser({ ...User, [e.target.name]: e.target.value });
+  };
 
-    // if () {
-    //   alert("Invalid Area Code!");
-    //   return;
-    // } else if () {
-    //   alert("Invalid Phone Number!");
-    //   return;
-    // } else if (password !== confirmPassword) {
-    //   alert("Passwords do not match!");
-    //   return;
-    // } else {
-    //   console.log("Full Name: " + firstName + " " + lastName);
-    //   console.log("Email: " + email);
-    //   console.log("Phone Number: " +areaCode + phoneNumber);
-    //   console.log("Date of Birth: " + dateOfBirth);
-    //   console.log("Password: " + password);
-    //   console.log("Address Line 1: " + addressLine1);
-    //   console.log("Address Line 2: " + addressLine2);
-    //   console.log("Town or City: " + townOrCity);
-    //   console.log("Postcode: " + postcode);
-    //   console.log("Terms and Conditions: " + termsAndConditions);
-    //   console.log("Privacy Policy: " + privacyPolicy);
-
-    //   navigate('/');
-    // }
-
-    if (password !== confirmPassword) {
-      alert("Passwords do not match!");
-      return;
+  const onSubmit = async (e) => {
+    if (password !== firstName) {
+      alert("Passwords do not match");
     } else {
-      console.log("Full Name: " + firstName + " " + lastName);
-      console.log("Email: " + email);
-      console.log("Phone Number: " +areaCode + phoneNumber);
-      console.log("Date of Birth: " + dateOfBirth);
-      console.log("Password: " + password);
-      console.log("Address Line 1: " + addressLine1);
-      console.log("Address Line 2: " + addressLine2);
-      console.log("Town or City: " + townOrCity);
-      console.log("Postcode: " + postcode);
-      console.log("Terms and Conditions: " + termsAndConditions);
-      console.log("Privacy Policy: " + privacyPolicy);
-
-      navigate('/');
+      e.preventDefault();
+      await axios.post("http://localhost:8080/Sign_Up", User);
+      navigate("/");
     }
-    
-  }
+  };
 
   return (
-    <div className='SignUp'>
-      <div className='SignUp-Form-Content'>
-
+    <div className="SignUp">
+      <div className="SignUp-Form-Content">
         <Link to="/accounts/login">
-          <button className="backButton" id = "backButton"> Back </button>
+          <button className="backButton" id="backButton">
+            {" "}
+            Back{" "}
+          </button>
         </Link>
         <br /> <br />
-
-        <div className='Container-SignUp-Form'>
-          <form onSubmit={submitForm}>
-            <input type="text" id="firstNameInput" name="firstName" placeholder="First Name" required />
-            <input type="text" id="lastNameInput" name="lastName" placeholder="Last Name" required />
+        <div className="Container-SignUp-Form">
+          <form onSubmit={(e) => onSubmit(e)}>
+            <input
+              type="text"
+              id="firstNameInput"
+              name="firstName"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => onInputChange(e)}
+            />
+            <input
+              type="text"
+              id="lastNameInput"
+              name="lastName"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => onInputChange(e)}
+            />
             <br /> <br />
-
-            <input type="email" id="emailInput" name="email" placeholder="Email" required />
+            <input
+              type="email"
+              id="emailInput"
+              name="eMail"
+              placeholder="Email"
+              value={eMail}
+              onChange={(e) => onInputChange(e)}
+            />
             <br /> <br />
-
-            <input type="text" id="areaCodeInput" name="areaCode" placeholder="Area Code" required />
-            <input type="text" id="phoneNumberInput" name="phoneNumber" placeholder="Phone Number" required />
-            <input type="date" id="dateOfBirthInput" name="dateOfBirth" placeholder="Date of Birth" required />
+            <input
+              type="number"
+              id="phoneNumberInput"
+              name="phoneNumber"
+              placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => onInputChange(e)}
+            />
+            <input
+              type="date"
+              id="dateOfBirthInput"
+              name="doB"
+              placeholder="Date of Birth"
+              value={doB}
+              onChange={(e) => onInputChange(e)}
+            />
             <br /> <br />
-
-            <input type="password" id="passwordInput" name="password" placeholder="Password" required />
-            <input type="password" id="confirmPasswordInput" name="confirmPassword" placeholder="Confirm Password" required />
+            <input
+              type="password"
+              id="passwordInput"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => onInputChange(e)}
+            />
+            <input
+              type="password"
+              id="confirmPasswordInput"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+            />
             <br /> <br />
-
-            <input type="text" id="addressLine1Input" name="addressLine1" placeholder="Address Line 1" required />
+            <input
+              type="text"
+              id="addressLine1Input"
+              name="addressLine1"
+              placeholder="Address Line 1"
+              value={addressLine1}
+              onChange={(e) => onInputChange(e)}
+            />
             <br /> <br />
-
-            <input type="text" id="addressLine2Input" name="addressLine2" placeholder="Address Line 2" required />
+            <input
+              type="text"
+              id="addressLine2Input"
+              name="addressLine2"
+              placeholder="Address Line 2"
+              value={addressLine2}
+              onChange={(e) => onInputChange(e)}
+            />
             <br /> <br />
-
-            <input type="text" id="townOrCityInput" name="townOrCity" placeholder="Town / City" required />
-            <input type="text" id="postcodeInput" name="postcode" placeholder="Postcode" required />
+            <input
+              type="text"
+              id="townOrCityInput"
+              name="addressTC"
+              placeholder="Town / City"
+              value={addressTC}
+              onChange={(e) => onInputChange(e)}
+            />
+            <input
+              type="text"
+              id="postcodeInput"
+              name="addressPostcode"
+              placeholder="Postcode"
+              value={addressPostcode}
+              onChange={(e) => onInputChange(e)}
+            />
             <br /> <br />
-
-            <input type="checkbox" id="termsAndConditionsInput" name="termsAndConditions" required />
-            <label for="termsAndConditions" id="termsAndConditionsLabel"> I agree to all the Terms & Conditions </label>
+            <input
+              type="checkbox"
+              id="termsAndConditionsInput"
+              name="termsAndConditions"
+            />
+            <label for="termsAndConditions" id="termsAndConditionsLabel">
+              {" "}
+              I agree to all the Terms & Conditions{" "}
+            </label>
             <br /> <br />
-
-            <input type="checkbox" id="privacyPolicyInput" name="privacyPolicy" required />
-            <label for="privacyPolicy" id="privacyPolicyLabel"> I agree to all the Privacy Policy </label>
+            <input
+              type="checkbox"
+              id="privacyPolicyInput"
+              name="privacyPolicy"
+            />
+            <label for="privacyPolicy" id="privacyPolicyLabel">
+              {" "}
+              I agree to all the Privacy Policy{" "}
+            </label>
             <br /> <br />
-
-            <button type="submit" id="submitButton"> Sign Up </button>
+            <button type="submit" id="submitButton">
+              {" "}
+              Sign Up{" "}
+            </button>
           </form>
         </div>
       </div>
 
-      <div className='SignUp-Info'>
-        <div className='SignUp-Info-Content'>
+      <div className="SignUp-Info">
+        <div className="SignUp-Info-Content">
           <h1>Sign Up</h1>
+
           <h2>Welcome to AquaPure</h2>
         </div>
       </div>
