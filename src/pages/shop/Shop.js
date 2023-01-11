843b2c238762019839cbccaea8d7b71a296c72df
ZancoFARRELL <92326067+ZancoFARRELL@users.noreply.github.com>
2023-01-11 19:40:23 +0000

    Adjustment to shop style

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -8,17 +8,28 @@ export default function Shop() {
 
   return (
     <div>
-      <div className="item_list">
+      
+      <div>
         <nav>
-          <ul>
+          <ul className="item_list">
             <li>
               <a href=".\item_pages\item_frame.js"><img alt="water" className="item_img" src={water}></img></a>
-              <p className="item_info">AquaPure Sport Bottle Blue</p>
+              <p className="item_text">AquaPure Sport Bottle Blue</p>
+              <p className="item_text">19.99</p>
               <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
             </li>
+
+            <li>
+              <a href=".\item_pages\item_frame.js"><img alt="water" className="item_img" src={water}></img></a>
+              <p className="item_text">AquaPure Sport Bottle Blue</p>
+              <p className="item_text">19.99</p>
+              <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
+            </li>
+
           </ul>
         </nav>
       </div>
+
     </div>
   );
 }
