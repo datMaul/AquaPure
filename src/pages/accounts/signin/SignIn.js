c332c587c98e9ea87af8ec0d1fa137b614197270
Eren Tastepe <2152973@brunel.ac.uk>
2023-01-13 15:37:17 +0300

    SignIn css and Signup css changes

--- src/pages/accounts/signin/SignIn.js
+++ src/pages/accounts/signin/SignIn.js
@@ -1,38 +1,72 @@
-import { Link } from 'react-router-dom';
-import './SignIn.css';
+import { Link } from "react-router-dom";
+import "./SignIn.css";
 
 export default function SignIn() {
   return (
-    <div className='SignIn'>
-      <div className='SignIn-Content'>
+    <div className="SignIn">
+      <div className="SignIn-Content">
         <h1> Sign In </h1>
-        <br/> <br/>
-        
-          <form>
-            <label for = "email"> Email: </label>
-            <input type = "email" id = "email" name = "email" placeholder = "Email" required/>
-            <br/> <br/>
-
-            <label for = "password"> Password: </label>
-            <input type = "password" id = "password" name = "password" placeholder = "Password" required/>
-            <br/> <br/>
-
-            <input type = "checkbox" id = "rememberMe" name = "rememberMe"/>
-            <label for = "rememberMe"> Remember Me </label>
-
-            <Link to = "/">
+        <form>
+          <div className="txt_field">
+            <input type="email" id="email" name="email" required />
+            <span />
+            <label for="email"> Email: </label>
+          </div>
+          <div className="txt_field">
+            <input type="password" id="password" name="password" required />
+            <span />
+            <label for="password"> Password: </label>
+          </div>
+          <div className="RememberMeBox">
+            <input type="checkbox" id="rememberMe" name="rememberMe" />
+            <label for="rememberMe"> Remember Me </label>
+          </div>
+          <br /> <br />
+          <div className="SignIn-Button-Section">
+            <Link to="/">
               {/* This button should check if the required data has inputted */}
-              <button type = "submit" className = "logInButton" id = "logInButton"> Log In </button>
+              <button type="submit" className="logInButton" id="logInButton">
+                {" "}
+                Log In{" "}
+              </button>
             </Link>
-            <br/> <br/>
-
-            <Link to = "/accounts/password/reset"> Forgot Password? </Link>
-            <br/> <br/>
-
-            <label for = "SigningUp"> Don't have an account </label>
-            <Link to = "/accounts/signup"> Sign Up Here</Link>
-          </form>
+          </div>
+          <div className="ForgotPassword-Section">
+            <label for="ForgotPassword"> Forgot Password? </label>
+            <Link to="/accounts/password/reset"> Click Here to Reset </Link>
+          </div>
+          <div className="SignUp-Section">
+            <label for="SigningUp"> Don't have an account </label>
+            <Link to="/accounts/signup"> Sign-Up Here</Link>
+          </div>
+          <br />
+        </form>
       </div>
-    </div> 
+    </div>
   );
+}
+
+{
+  /* <>
+  <div className="center">
+    <h1>Login</h1>
+    <form method="post">
+      <div className="txt_field">
+        <input type="text" required="" />
+        <span />
+        <label>Username</label>
+      </div>
+      <div className="txt_field">
+        <input type="password" required="" />
+        <span />
+        <label>Password</label>
+      </div>
+      <div className="pass">Forgot Password?</div>
+      <input type="submit" defaultValue="Login" />
+      <div className="SignUp">
+        Not a member? <a href="#">Signup</a>
+      </div>
+    </form>
+  </div>
+</>; */
 }
