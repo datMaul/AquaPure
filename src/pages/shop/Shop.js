ef3619ce0a310cd0f2aa65f7a2dd7b405a9e6d93
ZancoFARRELL <92326067+ZancoFARRELL@users.noreply.github.com>
2023-01-20 22:26:04 +0000

    styling changes to shop item

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -10,9 +10,15 @@ export default function Shop() {
         <nav>
           <ul className="item_list">
             <li className="item">
+<<<<<<< Updated upstream
               <Link to="/item"><img alt="water" className="item_img" src={water}/></Link>
               <p className="item_text">AquaPure Sport Bottle Blue</p>
               <p className="item_text">19.99</p>
+=======
+              <Link to={item_frame}><img alt="water" className="item_img" src={water}></img></Link>
+              <p className="item_title">AquaPure Sport Bottle Blue</p>
+              <p className="item_price">£19.99</p>
+>>>>>>> Stashed changes
               <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
             </li>
           </ul>
