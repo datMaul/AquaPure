ff00c7512f64ba55c009765125e8b8e5c24f6887
ZancoFARRELL <92326067+ZancoFARRELL@users.noreply.github.com>
2023-01-20 20:07:42 +0000

    Attempting to link item pages

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,5 +1,13 @@
 import "./Shop_Style.css";
 import water from "./shop_assets/water_bottle.PNG";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { Link } from "react-router-dom";
+import item_frame from "./item_pages/item_frame.js";
 export default function Shop() {
   return (
     
@@ -11,32 +19,12 @@ export default function Shop() {
           <ul className="item_list">
 
             <li className="item">
-              <a href=".\item_pages\item_frame.jsx"><img alt="water" className="item_img" src={water}></img></a>
+              <Link to={item_frame}><img alt="water" className="item_img" src={water}></img></Link>
               <p className="item_text">AquaPure Sport Bottle Blue</p>
               <p className="item_text">19.99</p>
               <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
             </li>
 
-            <li className="item">
-              <a href=".\item_pages\item_frame.js"><img alt="water" className="item_img" src={water}></img></a>
-              <p className="item_text">AquaPure Sport Bottle Blue</p>
-              <p className="item_text">19.99</p>
-              <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
-            </li>
-
-            <li className="item">
-              <a href=".\item_pages\item_frame.js"><img alt="water" className="item_img" src={water}></img></a>
-              <p className="item_text">AquaPure Sport Bottle Blue</p>
-              <p className="item_text">19.99</p>
-              <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
-            </li>
-
-            <li className="item">
-              <a href=".\item_pages\item_frame.js"><img alt="water" className="item_img" src={water}></img></a>
-              <p className="item_text">AquaPure Sport Bottle Blue</p>
-              <p className="item_text">19.99</p>
-              <button id="bottle" className="item_quick_add" type="button">Quick Add</button>
-            </li>
           </ul>
         </nav>
       </div>
--- src/pages/shop/item_pages/item_frame.js
+++ src/pages/shop/item_pages/item_frame.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
-import 'item_page.css';
+
 export default function item_frame() {
     return(
     <div>
-        <div className='page'>
+        
+        <div>
             hello test
         </div>
 
