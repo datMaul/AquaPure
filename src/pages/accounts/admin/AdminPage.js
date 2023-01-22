dac92553e0f35e5596644d9f9641cce079f721b5
Eren Tastepe <2152973@brunel.ac.uk>
2023-01-22 14:02:17 +0000

    Admin start

---
+++ src/pages/accounts/admin/AdminPage.js
@@ -0,0 +1,14 @@
+export default function AdminPage() {
+  return (
+    <div className="AdminPage">
+      <div className="AdminPage-Content">
+        <h1> Admin Page </h1>
+        <br /> <br />
+        <div className="AdminPage-NavBar">
+          {/* <Link>User Details</Link>
+            <Link>Test Kit</Link> */}
+        </div>
+      </div>
+    </div>
+  );
+}
