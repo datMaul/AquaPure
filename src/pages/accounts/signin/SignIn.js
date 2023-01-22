c0b9d5cbc467585bbf8084c3ed5909f92fde96ac
Berry1171 <118184408+Berry1171@users.noreply.github.com>
2023-01-22 01:22:36 +0000

    Test kit log Updates

--- src/pages/accounts/signin/SignIn.js
+++ src/pages/accounts/signin/SignIn.js
@@ -1,73 +1,44 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
 import './SignIn.css';
 
 export default function SignIn() {
-  const userRef=useRef()
-  const errRef=useRef()
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
 
-  const[user, setUser]=useState('');
-  const[pwd, setPwd]=useState('');
-  const[errMsg, setErrMsg]=useState('');
-  const[success, setSuccess]=useState(false);
-
-  useEffect(()=>{
-    useRef.current.focus();
-  },[])
-
-  useEffect(()=>{
-    setErrMsg('');
-  },[user,pwd])
-
-const handleSubmit=async(e)=>{
-  e.preventDefault();
-  console.log(user,pwd);
-  setUser('');
-  setPwd('');
-  setSuccess(true);
-}
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.post('http://localhost:8080/login', { email: user, password: pwd })
+      .then(res => {
+        //Sign In success
+        localStorage.setItem('token', res.data.token);
+        window.location = '/';
+        console.log("Ladies and Gentlemen, we got'em!");
+      })
+      .catch(err => {
+        //Sign In error
+        //setErrMsg(err.response.data.message);
+        setErrMsg("Wrong Inputs");
+        console.log("Error Chief!!!");
+      });
+  }
 
   return (
-    <>{success?(
-      <section>
-        <h1>You are Logged In!</h1>
-        <br/>
-        <p>Lets' Go Home!</p>
-      </section>):(
-    <div className='SignIn'>
-      <div className='SignIn-Content'>
-        <h1> Sign In </h1>
-        <br/> <br/>
-        <section>
-          <form onSubmit={handleSubmit}>
-            <p ref={errRef} className={errMsg?"errmsg":"offscreen"} aria-live="assertive">{errMsg}</p>
-            <label for = "email"> Email: </label>
-            <input type = "email" id = "email" name = "email" placeholder = "Email" ref={userRef} onChange={(e)=>setUser(e.target.value)} value={user} required/>
-            <br/> <br/>
-
-            <label for = "password"> Password: </label>
-            <input type = "password" id = "password" name = "password" placeholder = "Password"  onChange={(e)=>setPwd(e.target.value)} value={pwd} required/>
-            <br/> <br/>
-
-            <input type = "checkbox" id = "rememberMe" name = "rememberMe"/>
-            <label for = "rememberMe"> Remember Me </label>
-
-            <Link to = "/">
-              {/* This button should check if the required data has inputted */}
-              <button type = "submit" className = "logInButton" id = "logInButton"> Log In </button>
-            </Link>
-          </form>
-            <br/> <br/>
-
-            <Link to = "/accounts/password/reset"> Forgot Password? </Link>
-            <br/> <br/>
-
-            <label for = "SigningUp"> Don't have an account </label>
-            <Link to = "/accounts/signup"> Sign Up Here</Link>
-      </section>
-      </div>
-    </div> )}
-    </>
+    // ...
+    <form onSubmit={handleSubmit}>
+      <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+      <label htmlFor="email"> Email: </label>
+      <input type="email" id="email" name="email" placeholder="Email" onChange={(e) => setUser(e.target.value)} value={user} required />
+      <br /> <br />
+      <label htmlFor="password"> Password: </label>
+      <input type="password" id="password" name="password" placeholder="Password" onChange={(e) => setPwd(e.target.value)} value={pwd} required />
+      <br /> <br />
+      <input type="checkbox" id="rememberMe" name="rememberMe" />
+      <label htmlFor="rememberMe"> Remember Me </label>
+      <button type="submit" className="logInButton" id="logInButton"> Log In </button>
+    </form>
+    // ...
   );
 }
