ec259c535ed98415b80ed2fd659589855d6c5964
Eren Tastepe <2152973@brunel.ac.uk>
2023-02-04 19:14:00 +0000

    TestKitEntry

--- src/pages/accounts/accountPageSections/TestkitEntry.js
+++ src/pages/accounts/accountPageSections/TestkitEntry.js
@@ -65,6 +65,96 @@ export default function TestkitEntry() {
 
       <div className="TestKitEntry-Content">
         <h2> Testkit Entry </h2>
+
+        {/* pH, Total Alkalinity, Total Hardness, Nitrite, Nitrate, Lead, Manganese, Coliform Bacteria */}
+
+        <form
+        // onSubmit={(e) => onSubmit(e)}
+        >
+          <div>
+            <label> pH : </label>
+            <input
+              type={"number"}
+              placeholder="pH"
+              name="pH"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <label> Total Alkalinity : </label>
+            <input
+              type={"number"}
+              placeholder="Total Alkalinity"
+              name="totalAlkalinity"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <label> Total Hardness : </label>
+            <input
+              type={"number"}
+              placeholder="Total Hardness"
+              name="totalHardness"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <label> Nitrite : </label>
+            <input
+              type={"number"}
+              placeholder="Nitrite"
+              name="nitrite"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <label> Nitrate : </label>
+            <input
+              type={"number"}
+              placeholder="Nitrate"
+              name="nitrate"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <label> Lead : </label>
+            <input
+              type={"number"}
+              placeholder="Lead"
+              name="lead"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <label> Manganese : </label>
+            <input
+              type={"number"}
+              placeholder="Manganese"
+              name="manganese"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <label> Coliform Bacteria : </label>
+            <input
+              type={"number"}
+              placeholder="Coliform Bacteria"
+              name="coliformBacteria"
+              // value={}
+              // onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <button id="submitButton" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
