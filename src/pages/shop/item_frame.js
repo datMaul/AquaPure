2508aa5656b25a59302185218d40ea334f09ede3
ZancoFARRELL <92326067+ZancoFARRELL@users.noreply.github.com>
2023-02-04 13:23:43 +0000

    Style changes to item page frames

--- src/pages/shop/item_frame.js
+++ src/pages/shop/item_frame.js
@@ -3,16 +3,18 @@ import water from "./shop_assets/water_bottle.PNG";
 export default function shope_item() {
     return(
       <div>
-        <div className="page">
+        <div className="page_item">
           <center>
             <img alt="water" className="item_page_img" src={water}/>
           </center>
         
           <div>
-            <h1>AquaPure Sport Bottle</h1>
-            <h2>£19.99</h2>
-            <h3>This bottle was made from china</h3>
+            <h1 className="title item_page_text">AquaPure Sport Bottle</h1>
+            <h2 className="price item_page_text">£19.99</h2>
+            <h3 className="desc item_page_text">COLOUR:</h3>
+            <button className="add" type="button">Add to Cart</button>
           </div>
+
         </div>
       </div>
     );
