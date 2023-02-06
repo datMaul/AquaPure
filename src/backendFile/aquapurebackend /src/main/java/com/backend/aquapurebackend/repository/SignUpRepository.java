6af6da26a9f6612086bce73f06fd41c16aa234dd
Berry1171 <118184408+Berry1171@users.noreply.github.com>
2023-02-06 22:26:58 +0000

    Backend issues

--- src/backendFile/aquapurebackend /src/main/java/com/backend/aquapurebackend/repository/SignUpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.aquapurebackend.repository;
-
-import com.backend.aquapurebackend.model.SignUp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SignUpRepository extends JpaRepository <SignUp, Long> {
-    SignUp findByeMail(String eMail);
-}
